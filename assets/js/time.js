7dfe9699fa7812db7ea2c8e35289f916cca232f7
jiangfan01 <1431037397@qq.com>
2022-10-18 21:40:25 +0800

    echo

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,6 +1,12 @@
 window.onload = function (){
-    let now_time = document.querySelector(".now-time")
+    let year = document.querySelector(".year")
+    let mouth = document.querySelector(".mouth")
+    let day = document.querySelector(".day")
     moment.locale('zh-cn')
-    let time = moment().format('LL');
-    now_time.innerHTML = time
+    let now_year = moment().format('YYYY 年'); //年
+    let now_mouth = moment().format('M 月'); //月
+    let now_day = moment().format('D 日'); //日
+    year.innerHTML = now_year
+    mouth.innerHTML = now_mouth
+    day.innerHTML = now_day
 }
